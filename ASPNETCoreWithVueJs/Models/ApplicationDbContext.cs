e193590922af6b80d6ac62c9009966d2d4a09607
Ishan Randeniya <irandeniya05@gmail.com>
2019-10-12 13:34:33 +0530

    Add files via upload

---
+++ ASPNETCoreWithVueJs/Models/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreWithVueJs.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        readonly DbContextOptions<ApplicationDbContext> options;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> _options) : base(_options)
+        {
+            this.options = _options;
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (options == null)
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS17; Database=VueCRUDApp; Trusted_Connection=true;");
+        }
+    }
+}
---
+++ ASPNETCoreWithVueJs/Scripts/Product/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="product">
+        <h1>Product Create -Vue</h1>
+        <p>
+            <a :href="CreateProductUrl">Create New</a>
+        </p>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>
+                        Name
+                    </th>
+                    <th>
+                        Price
+                    </th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in Products">
+                    <td>
+                        {{item.Name}}
+                    </td>
+                    <td>
+                        {{item.Price}}
+                    </td>
+                    <td>
+                        <a :href="EditUrl+'/'+item.Id">Edit</a>|
+                        <a :href="DetailsUrl+'/'+item.Id">Details</a> |
+                        <a :href="DeleteUrl+'/'+item.Id">Delete</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+<script>
+    export default {
+        name: "product-create-component",
+        props: {
+            CreateProductUrl: String,
+            DeleteUrl: String,
+            EditUrl: String,
+            DetailsUrl: String,
+            Products: Array
+        }
+    };
+</script>
